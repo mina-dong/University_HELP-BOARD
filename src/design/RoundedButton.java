f8bbcc0253278d6ead5ad5c76b9d087df2bf9064
Mina Dong <82349462+mina-dong@users.noreply.github.com>
2022-12-28 19:06:44 +0900

    Add files via upload

---
+++ src/design/RoundedButton.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package design;
+
+import java.awt.Desktop.Action;
+import javax.swing.Icon;
+import javax.swing.JButton;
+
+/**
+ *
+ * @author https://the-illusionist.me/42
+ */
+public final class RoundedButton extends JButton {
+
+    public RoundedButton() {
+        super();
+        decorate();
+    }
+
+    public RoundedButton(String text) {
+        super(text);
+        decorate();
+    }
+
+    public RoundedButton(Action action) {
+        super(/*action*/);
+        decorate();
+    }
+
+    public RoundedButton(Icon icon) {
+        super(icon);
+        decorate();
+    }
+
+    public RoundedButton(String text, Icon icon) {
+        super(text, icon);
+        decorate();
+    }
+
+    protected void decorate() {
+        setBorderPainted(false);
+        setOpaque(false);
+    }
+}
